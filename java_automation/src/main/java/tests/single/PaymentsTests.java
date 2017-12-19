5ee440c1a7132ee1c2c36117881cfd56979a1524
Imtiaz Hossain <imtiaz.hossain@youvisit.com>
2017-12-19 17:27:19 -0500

    skip payments test since swarovski experience no longer has payments data

--- java_automation/src/main/java/tests/single/PaymentsTests.java
+++ java_automation/src/main/java/tests/single/PaymentsTests.java
@@ -13,25 +13,25 @@ public class PaymentsTests extends BaseTest {
     @Severity(SeverityLevel.NORMAL)
     @Test(groups = { "SMOKE"})
     public void submitting() throws Exception {
-        swarovskiPage = pageObjectsHandler.getSwarovskiPage();
-        swarovskiPage.getPageLoadedLocator();
-        swarovskiPage.clickSkipIntro();
-        swarovskiPage.clickApplication();
-        swarovskiPage.clickHotspot();
-        swarovskiPage.clickBuy();
-        swarovskiPage.clickCheckout();
-        swarovskiPage.inputName("QA Tester");
-        swarovskiPage.inputAddress("Juan's House");
-        swarovskiPage.inputZipcode("10001");
-        swarovskiPage.inputEmail("testingyv@gmail.com");
-        swarovskiPage.clickContinue();
-        swarovskiPage.inputCardName("QA Tester");
-        swarovskiPage.inputCardNumber("4242424242424242");
-        swarovskiPage.inputExpDate("1030");
-        swarovskiPage.inputCVC("123");
-        swarovskiPage.clickSubmit();
-        swarovskiPage.clickPlaceOrder();
-        swarovskiPage.clickContinueExp();
+//        swarovskiPage = pageObjectsHandler.getSwarovskiPage();
+//        swarovskiPage.getPageLoadedLocator();
+//        swarovskiPage.clickSkipIntro();
+//        swarovskiPage.clickApplication();
+//        swarovskiPage.clickHotspot();
+//        swarovskiPage.clickBuy();
+//        swarovskiPage.clickCheckout();
+//        swarovskiPage.inputName("QA Tester");
+//        swarovskiPage.inputAddress("Juan's House");
+//        swarovskiPage.inputZipcode("10001");
+//        swarovskiPage.inputEmail("testingyv@gmail.com");
+//        swarovskiPage.clickContinue();
+//        swarovskiPage.inputCardName("QA Tester");
+//        swarovskiPage.inputCardNumber("4242424242424242");
+//        swarovskiPage.inputExpDate("1030");
+//        swarovskiPage.inputCVC("123");
+//        swarovskiPage.clickSubmit();
+//        swarovskiPage.clickPlaceOrder();
+//        swarovskiPage.clickContinueExp();
     }
 }
 
